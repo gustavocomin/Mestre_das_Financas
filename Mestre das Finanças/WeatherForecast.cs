83dd658be22e7e4e5133a4b6d9e9eaba8c25380e
gustavo.comin <gustavofcomin@gmail.com>
2023-04-17 14:14:55 -0300

    Add project files.

---
+++ Mestre das Finanças/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Mestre_das_Finanças
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
